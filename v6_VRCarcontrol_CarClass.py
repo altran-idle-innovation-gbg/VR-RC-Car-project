c4a243847c8554730c6b439dfcb45f6f3bec12c9
joakim Garcia Wernersson <joakim.garcia@altran.com>
2017-10-04 13:12:18 +0200

    Updated.

--- v6_VRCarcontrol_CarClass.py
+++ v6_VRCarcontrol_CarClass.py
@@ -6,10 +6,11 @@ import os
 import sys
 import math
 from pygame.locals import *
-###-------------------Accelerometer--------------------
 import string
 import re
 import socket, traceback
+
+### -------------------Accelerometer--------------------
 
 host = ''
 port = 5555
@@ -18,21 +19,13 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 s.bind((host, port))
-###--------------------------------------------------
+###------------------- End Accelerometer -------------------------------
 
-
-# IP_ADDRESS = "192.168.0.2" # ip address of host used for video stream
-# RASPICAM_ON = "raspivid -t 999999 -w 1280 -h 720 -sa -50 -br 60 -co 20 -fps 24 -b 3000000 -o - | gst-launch-1.0 -e -vvv fdsrc ! h264parse ! rtph264pay pt=96 config-interval=5 ! udpsink host=" + IP_ADDRESS + " port=8160&"
-# RASPICAM_OFF = "sudo killall -9 gst-launch-1.0"
-PHOTOS_DIR = "/home/pi/Desktop/Tank_photos"  # save photos to a any directory
-SNAP_PHOTO = "raspistill -w 1920 -h 1080 -t 500 -o " + PHOTOS_DIR + "/"
-IMAGE_NUMBERS_TXT_FILE = "image_numbers.txt"  # text file to append number to 'image.jpg'
-EMAIL_TO = "altran.innovation.gbg@gmail.com"
 # -------------------- GPIO INITIATION ------------------------
-GPIO.setmode(GPIO.BOARD)
-# Below 4 rows just tells the RPi what theese pins are output pinns =(pins to send signals to the H-brige with)
+
+GPIO.setmode(GPIO.BOARD)  # Below 4 rows just tells the RPi what theese pins are output pinns =(pins to send signals to the H-brige with)
 GPIO.setup(7, GPIO.OUT)  # EN1 controls left hand side wheels (H-bridge connector J1 pin1)
-GPIO.setup(11, GPIO.OUT)  # EN2 controles right hand side wheelsa (H-bridge connector J1 pin7)
+GPIO.setup(11, GPIO.OUT)  # EN2 controls right hand side wheelsa (H-bridge connector J1 pin7)
 GPIO.setup(13, GPIO.OUT)  # DIR1 LH True=Forward & False=Backward
 GPIO.setup(15, GPIO.OUT)  # DIR2 RH True=Forward & False=Backward
 
@@ -40,6 +33,7 @@ GPIO.setup(12, GPIO.OUT)  # Sets the pin 12 as an output/signaling pin for the S
 GPIO.setwarnings(False)
 GPIO.output(7, False)
 GPIO.output(11, False)
+
 # ------------------ END GPIO INITIATION -----------------------
 
 # -------------------Start Car Class-------------------------------
@@ -97,8 +91,8 @@ stop = False
 
 
 def drive_forward():
-    GPIO.output(7, False)  # EN1 Enable RH wheels to spin
-    GPIO.output(11, False)  # EN2 Enable LH wheels to spin
+    GPIO.output(7, False)  # EN1 Disable RH wheels to spin
+    GPIO.output(11, False)  # EN2 Disable LH wheels to spin
     GPIO.output(13, True)  # Enable RH wheels to spin forward
     GPIO.output(15, True)  # Enable LH wheels to spin forward
     GPIO.output(7, True)  # EN1 Enable RH wheels to spin
@@ -106,29 +100,29 @@ def drive_forward():
 
 
 def drive_backward():
-    GPIO.output(7, False)  # EN1 Enable RH wheels to spin
-    GPIO.output(11, False)  # EN2 Enable LH wheels to spin
+    GPIO.output(7, False)  # EN1 Disable RH wheels to spin
+    GPIO.output(11, False)  # EN2 Disable LH wheels to spin
     GPIO.output(13, False)  # Enable RH wheels to spin backwards
     GPIO.output(15, False)  # Enable LH wheels to spin backwards
     GPIO.output(7, True)  # EN1 Enable RH wheels to spin
     GPIO.output(11, True)  # EN2 Enable LH wheels to spin
 
 
-def drive_left_forward():
-    GPIO.output(7, False)  # EN1 Enables RH wheels to spin
+def drive_left_pivot():
+    GPIO.output(7, False)  # EN1 Disable RH wheels to spin
     GPIO.output(11, False)  # EN2 Disable LH wheels to spin
     GPIO.output(13, True)  # Enabels RH wheels to spin forward
     GPIO.output(15, False)  # Enabels LH wheels to spin backwards
     GPIO.output(7, True)  # EN1 Enables RH wheels to spin
-    GPIO.output(11, False)  # EN2 Disable LH wheels to spin
+    GPIO.output(11, True)  # EN2 Enables LH wheels to spin
 
 
-def drive_right_forward():
+def drive_right_pivot():
     GPIO.output(7, False)  # EN1 Disable RH wheels to spin
     GPIO.output(11, False)  # EN2 Enables LH wheels to spin
     GPIO.output(13, False)  # Enabels RH wheels to spin backwards
     GPIO.output(15, True)  # Enabels LH wheels to spin forward
-    GPIO.output(7, False)  # EN1 Disable RH wheels to spin
+    GPIO.output(7, True)  # EN1 Enables RH wheels to spin
     GPIO.output(11, True)  # EN2 Enables LH wheels to spin
 
 
@@ -142,47 +136,40 @@ def stop_all():
 
 # ---END-Define class with GPIO instructions for driving---------
 
-# ---------------------------------------------------
+
 # Define class with GPIO instructions for driving
 
+# --------Servo Movements--------------
+
+
 def printit():
-    #####------------------------servo movement--------------------
     while runner:
 
         runs = False
 
         message, address = s.recvfrom(8192)
-
         var = message.split()
         temp2 = str(var[3].strip())
         temp3 = re.sub('[^0-9.-]', '', temp2)
-
         print (temp3)
-
         acc = float(temp3)
 
         if (-3 < acc < -1):
             runs = True
-            acc2 = 4  # left
+            acc2 = 5.5  # left
         elif (1 < acc < 3):
             runs = True
-            acc2 = 11  # right
+            acc2 = 9.5  # right
         elif (-1 <= acc <= 1):
             runs = True
-            acc2 = 8  # middle
+            acc2 = 7.5  # middle
         else:
             runs = False
 
         try:
             if runs:
-                servoPin = 16  # Servo signaling pin
-                pwm = gpio.PWM(servoPin, 50)
-                pwm.start(acc2)  # Makes the servo turn
-                time.sleep(1)  # The time for the servo to turn
-                pwm.stop()  # Stop the servo
+                pwm.ChangeDutyCycle(acc2)  # Makes the servo turn
                 print(acc2)
-
-
         except (KeyboardInterrupt, SystemExit):
             raise
         except:
@@ -191,52 +178,57 @@ def printit():
 
 #####----------------------END servo movement--------------------
 
-##    def stopDriving():
-##        gpio.output(7,False) # EN1 Disable RH wheels to spin
-##        gpio.output(11,False) # EN2 Disable LH wheels to spin
-##        running = False
+# --------------------- Driving direction list ----------------------
 
+driving_direction_list = {'forward': drive_forward, 'backward': drive_backward,
+                        'left': drive_left_pivot, 'right': drive_right_pivot, 'stop': stop_all}
 
+# --------------------- End Driving Direction List ------------------
 
-def driveDirection(axis0, axis1):
+## -------------------- Start joystick control ---------------------
+
+def drive_direction(axis0, axis1):
     axis0 = int(round(axis0))
     axis1 = int(round(axis1))
     if axis0 == 0 and axis1 == -1:
-        driveForward()
+        the_car.set_driving_direction('forward')
         print ("Going Forward")
-    if axis0 == 0 and axis1 == 1:
-        driveBackward()
+    elif axis0 == 0 and axis1 == 1:
+        the_car.set_driving_direction('backward')
         print ("Going Backward")
-    if axis0 == -1 and axis1 == 0:
-        driveLeftForward()
+    elif axis0 == -1:
+        the_car.set_driving_direction('left')
         print ("Going LeftForward")
-    if axis0 == 1 and axis1 == 0:
-        driveRightForward()
+    elif axis0 == 1:
+        the_car.set_driving_direction('right')
         print ("Going RightForward")
-    if axis0 == -1 and axis1 == 1:
+    else:
+        stop_all()
+'''   elif axis0 == -1 and axis1 == 1:
         driveLeftBackward()
         print ("Going LeftBackward")
-    if axis0 == 1 and axis1 == 1:
+    elif axis0 == 1 and axis1 == 1:
         driveRightBackward()
         print ("Going RightBackward")
-    if axis0 == -1 and axis1 == 0:
+    elif axis0 == -1 and axis1 == 0:
         driveLeftPivot()
         print ("LeftPivot")
-    if axis0 == 1 and axis1 == 0:
+    elif axis0 == 1 and axis1 == 0:
         driveRightPivot()
-        print ("RightPivot")
-    if axis0 == 0 and axis1 == 0:
-        stopAll()
+        print ("RightPivot")'''
+
+
+## ------------------- End Joystick control -------------------
 
 # ----------Define quit game class -----------------
 def stop_program():
      """shuts down all running components of program"""
-     '''
+
      try:
         joyStick.quit()
      except:
         pass
-        '''
+
      stop_all()
      pwm.stop()
      GPIO.cleanup()
@@ -263,7 +255,6 @@ def main():
     controls by pressing the <space> key. while using keyboard controls,
     speed can be set using number keys 1 - 9'''
 
-    #    os.system(RASPICAM_ON)
     the_car = CAR()
     stop = False
     while True:
@@ -280,17 +271,17 @@ def main():
                 if event.type == pygame.JOYAXISMOTION:
                     axis0 = joyStick.get_axis(0)
                     axis1 = joyStick.get_axis(1)
-                    driveDirection(axis0, axis1)
-                ##                elif event.type == pygame.JOYBUTTONDOWN:
-                ##                    if event.button == 1: #button C on joystick-VRBOX for camera turn LEFT
-                ##                        servoLeft()
-                ##                        print ("Camera turn left")
-                ##                    if event.button == 4: #button A on joystick-VRBOX for camera point STRAIGHT
-                ##                        servoStraight()
-                ##                        print ("Camera point straight forward")
-                ##                    if event.button == 3: #button D on joystick-VRBOX for camera turn RIGHT
-                ##                        servoRight()
-                ##                        print ("Camera turn right")
+                    drive_direction(axis0, axis1)
+                elif event.type == pygame.JOYBUTTONDOWN:
+                    if event.button == 1: #button C on joystick-VRBOX for camera turn LEFT
+                        the_car.servo_turn_left()
+                        print ("Camera turn left")
+                    if event.button == 4: #button A on joystick-VRBOX for camera point STRAIGHT
+                        the_car.set_camera_direction(7.5)
+                        print ("Camera point straight forward")
+                    if event.button == 3: #button D on joystick-VRBOX for camera turn RIGHT
+                        the_car.servo_turn_right()
+                        print ("Camera turn right")
                 elif event.type == pygame.KEYDOWN:
                     if event.key == 32:
                         running = False
@@ -301,30 +292,52 @@ def main():
         stopAll()
 
         running = True
-        speed = 350
         while running:
             time.sleep(.02)
-            if stop == True:
+            if stop:
                 break
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
-                    if event.key in MOVE_IT:  # keys <w> <a> <s> <d>
-                        MOVE_IT[event.key](speed)
-                    elif event.key == 102:  # key <f>
-                        lights = headLights(lights)
-                    elif event.key in SPEED:  # keys <1 - 9>
-                        speed = SPEED[event.key]
-                    elif event.key == 99:  # key <c>
-                        takePic(getFileName(IMAGE_NUMBERS_TXT_FILE))
-                    elif event.key == 118:  # key <v>
-                        emailPic(EMAIL_TO)
-                    elif event.key == 32:  # key <space>
-                        running = False
-                        runner = False
-                    elif event.key == K_ESCAPE:  # key <escape>
-                        stop = Truewsw
-                elif event.type == pygame.KEYUP:
-                    stopAll()
+                    if event.key == 32:  # key <SPACE> Stop motors / Break
+                        stop_all()
+                    if event.key == K_w:  # key <W> Move forward
+                        the_car.set_driving_direction('forward')
+                        print(the_car.get_driving_direction())
+                    if event.key == K_s:  # key <S> Move backward
+                        the_car.set_driving_direction('backward')
+                        print(the_car.get_driving_direction())
+                    if event.key == K_a:  # key <A> Move left
+                        the_car.set_driving_direction('left')
+                        print(the_car.get_driving_direction())
+                    if event.key == K_d:  # key <D> Move right
+                        the_car.set_driving_direction('right')
+                        print(the_car.get_driving_direction())
 
-main()
-quit()
+                    if event.key == K_q:  # key <Q> Turn servo left
+                        the_car.set_camera_direction(5.5)
+                    if event.key == K_r:  # key <R> Turn servo right
+                        the_car.set_camera_direction(9.5)
+                    if event.key == K_e:  # key <E> Turn servo straight forward
+                        the_car.set_camera_direction(7.5)
+
+                    if event.key == K_c:
+                        the_car.servo_turn_left()
+                        print('Camera Direction DC = ', the_car.get_camera_direction())
+                    if event.key == K_v:
+                        the_car.servo_turn_right()
+                        print('Camera Direction DC = ', the_car.get_camera_direction())
+
+                    if event.key == K_ESCAPE:  # key <Esc> QUIT
+                        stop = True
+                elif event.type == pygame.KEYUP:
+                    the_car.set_driving_direction('stop')
+                    print(the_car.get_driving_direction())
+
+                driving_direction_list[the_car.get_driving_direction()]()
+                pwm.ChangeDutyCycle(the_car.get_camera_direction())
+                time.sleep(0.05)
+try:
+    main()
+except:
+    pass
+stop_program()
