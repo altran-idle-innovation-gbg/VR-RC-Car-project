3c0294588579cce2214fd93b798d9de9ba54585a
bergfalk <arre@bergfalk.com>
2017-10-04 14:41:57 +0200

    New File for Wireless IMU test

---
+++ Test scripts/wireless_IMU_test.py
@@ -0,0 +1,20 @@
+# -------------------------------------------------------
+import socket, traceback
+
+host = '10.46.2.174'
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+s.bind((host, port))
+
+while 1:
+    try:
+        message, address = s.recvfrom(8192)
+        print message
+    except (KeyboardInterrupt, SystemExit):
+        raise
+    except:
+        traceback.print_exc()
+# -------------------------------------------------------
